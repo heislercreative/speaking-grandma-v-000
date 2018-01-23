98bc60449d15405be27bf498f8360d408cdc107a
Brennan Heisler <learn@heislercreative.com>
2018-01-23 00:53:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@
 def speak_to_grandma(say)
   if say == say.capitalize
     "NO, NOT SINCE 1938!"
+  elsif say == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
